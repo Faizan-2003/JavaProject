a861d11c21e214ed66410254688478a1fd2d6f95
Muhammad Faizan <701765@student.inholland.nl>
2023-10-14 22:13:42 +0200

    UI changes

--- Java-End-Assignment/src/main/java/com/example/javaendassignment/Controllers/LoginController.java
+++ Java-End-Assignment/src/main/java/com/example/javaendassignment/Controllers/LoginController.java
@@ -1,4 +1,12 @@
 package com.example.javaendassignment.Controllers;
 
+import javafx.event.ActionEvent;
+import javafx.scene.input.InputMethodEvent;
+
 public class LoginController {
+    public void onLoginButtonClick(ActionEvent actionEvent) {
+    }
+
+    public void onPasswordTextChange(InputMethodEvent inputMethodEvent) {
+    }
 }
